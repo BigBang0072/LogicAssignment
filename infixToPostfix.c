3066783ae9f166f90d72a262ac34e080a8c5994a
Abhinav Kumar <abhinavkumar.sieger@gmail.com>
2017-10-01 00:24:13 +0530

    Commented and a Bug fixed in Task1

--- infixToPostfix.c
+++ infixToPostfix.c
@@ -12,23 +12,34 @@ int main(){
 	char postStack[lengthInfix];
 	initializeBuffer(lengthInfix,postStack);
 	infixToPostfix(lengthInfix,infix,&lengthPostStack,postStack);
-	for(int i=0;i<10;i++){
+	for(int i=0;i<lengthInfix;i++){
 		printf("%c ",postStack[i]);
 	}
+	printf("\n%d\n",lengthPostStack);
 	return 0;
 }
 
 void infixToPostfix(int lengthInfix,char infix[],int* lengthPostStack,char postStack[]){
-	/*
+	/*  INPUT:1.Number of Character in Infix array.
+			  2.Infix character Array.
+			  3.Pointer to length of Postfix Form (for iterating over in next task.)
+			  4.To get the Postfix form in form of Character Array.
+
+	    OUTPUT: None.
+
 
 	*/
+
+	//WORKSPACE CREATION
 	//Initializing the local Buffer for our operation.
+	//This will act as storing the operators and parenthesis coming in form Infix fromat.
+	int wtos=-1; // This is a top of Stack counter variable.
 	char workStack[lengthInfix];
 	initializeBuffer(lengthInfix,workStack);
 
 
 	//Traversing the infix and converting it to the postfix.
-	int wtos=-1,postTos=-1;
+	int postTos=-1; //This is top of Stack variable for working on Postfix array.
 	int previousRank=0,currentRank=0;
 	for(int i=0;i<lengthInfix;i++){
 		char data=infix[i];//making a local copy of the current character in Infix.
@@ -36,12 +47,16 @@ void infixToPostfix(int lengthInfix,char infix[],int* lengthPostStack,char postS
 		//If out character is a operation.
 		if(data=='~' || data=='*' || data=='+' || data=='>'){
 			currentRank=mapRankToOperator(data);
+			//If the rank of scanned operator in greater than the last operator scanned.
 			if(currentRank>previousRank){
 				wtos++;
 				workStack[wtos]=data;
 				previousRank=currentRank;
 			}
+			//If the current scannd operator is of less rank than last one
 			else{
+				//put the operator to the Postfix Stack until rank of the 
+				//operator in workStack becomes less than scanned rank.
 				do{
 					postTos++;
 					postStack[postTos]=data;
@@ -49,19 +64,27 @@ void infixToPostfix(int lengthInfix,char infix[],int* lengthPostStack,char postS
 					char stackData=workStack[wtos];
 					previousRank=mapRankToOperator(stackData);
 				}while(currentRank<=previousRank);
+				//Now finally putting the scanned operator in workStack.
 				wtos++;
 				workStack[wtos]=data;
 				previousRank=currentRank;
 			}
 
 		}
+
+
+
 		//If we encounter a parnthesis which referes to a specialized subtree in Binary tree representation.
 		else if(data=='('){
+			//The opening parenthesis represent private grouping of operation.
 			wtos++;
 			workStack[wtos]=data;
 		}
 		else if(data==')'){
+			//So until we again encounter the close we have to do the conversion
+			//as if doing it for independent problem.
 			char stackData=workStack[wtos];
+			//Finally as subproblem ends we put them in Postfix Stack.
 			while(stackData!='('){
 				postTos++;
 				postStack[postTos]=stackData;
@@ -71,11 +94,20 @@ void infixToPostfix(int lengthInfix,char infix[],int* lengthPostStack,char postS
 			wtos--;
 		}
 
+
+
+
 		else{
+			//If operands are encountered, then directly push them to the Postfix Stack.
 			postTos++;
 			postStack[postTos]=data;
 		}
 	}
+
+
+
+	//Now putting the rest of left operations present in the workStack to
+	// the Postfix Stack.
 	while(wtos!=-1){
 		char data=workStack[wtos];
 		wtos--;
@@ -83,6 +115,8 @@ void infixToPostfix(int lengthInfix,char infix[],int* lengthPostStack,char postS
 		postStack[postTos]=data;
 	}
 
+	//Updating the length of the PostOrder stack for the furthur 
+	// iterating in the array in next task.
 	*lengthPostStack=postTos+1;
 }
 
@@ -108,7 +142,7 @@ int mapRankToOperator(char operation){
 	else if(operation =='>'){
 		return 1;
 	}
-	return -1;//to take care of the ( which is pushed inside the work buffer.
+	return -1;//to take care of the "("" which is pushed inside the work buffer.
 	// The popping of work Stack of operator will only go until "(" cuz it will be in less rank
 	//than any of the current operator.
 }
